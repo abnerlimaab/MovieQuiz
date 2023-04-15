b732ebc54850854f9b28b68b30046a12029a0732
Abner Lima <abnerlima.1997@gmail.com>
2023-04-15 13:06:26 -0300

    Tocando arquivos de audio com AVAudioPlayer

--- MovieQuiz/ViewController.swift
+++ MovieQuiz/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -16,10 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var ivQuiz: UIImageView!
     
     var quizManager: QuizManager!
+    var quizPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,6 +28,7 @@ class ViewController: UIViewController {
         
         quizManager = QuizManager()
         getNewQuiz()
+        startTimer()
     }
     
     func getNewQuiz() {
@@ -35,8 +37,42 @@ class ViewController: UIViewController {
         for i in 0..<round.options.count {
             btOptions[i].setTitle(round.options[i].name, for: .normal)
         }
+        
+        playQuiz()
     }
-
+    
+    func startTimer() {
+        viTimer.frame = view.frame
+        
+        UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear, animations: {
+            self.viTimer.frame.size.width = 0
+            self.viTimer.frame.origin.x = self.view.center.x
+        }) { success in
+            self.gameOver()
+        }
+    }
+    
+    func gameOver() {
+        performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        quizPlayer.stop()
+    }
+    
+    @IBAction func playQuiz() {
+        guard let round = quizManager.round else {return}
+        ivQuiz.image = UIImage(named: "movieSound")
+        
+        if let url = Bundle.main.url(forResource: "quote\(round.quiz.number)", withExtension: "mp3") {
+            do {
+                quizPlayer = try AVAudioPlayer(contentsOf: url)
+                quizPlayer.volume = 1
+                quizPlayer.play()
+                quizPlayer.delegate = self
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     @IBAction func showHideSoundBar(_ sender: UIButton) {
         viSoundBar.isHidden = !viSoundBar.isHidden
     }
@@ -54,3 +90,8 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        ivQuiz.image = UIImage(named: "movieSoundPause")
+    }
+}
